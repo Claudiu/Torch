7b24892884d0d2d75b2c6956ace8c0d501a9fa54
Claudiu T <clawgm@gmail.com>
2012-08-06 18:49:11 +0300

    deleted:    Modules/HTTP/application.hpp 	modified:   main.cpp 	modules/

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "Modules/HTTP/application.hpp"
+#include "modules/http.hpp"
 
 int main(int argc, char const *argv[])
 {
 	Torch::HTTP::application app;
 	
 	app.get("STDIO");
-	
+
 	app.listen(8080);
 
 	return 0;
