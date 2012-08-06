6ff8456492e25bae3dda780d41596924d724d872
Claudiu T <clawgm@gmail.com>
2012-08-06 19:53:27 +0300

    Added callback functions to http

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "modules/http.hpp"
 
+namespace Routes {
+	void index() {
+		std::cout << "Reached Index";
+	}
+};
+
 int main(int argc, char const *argv[])
 {
 	Torch::HTTP::application app;
 	
-	app.get("STDIO");
-
-	app.listen(8080);
+	app.get("/", Routes::index);
+	app.get("/out", Routes::index);
 
 	return 0;
 }
--- modules/HTTP/application.hpp
+++ modules/HTTP/application.hpp
@@ -1,13 +1,17 @@
 #ifndef __HTTP_APPLICATION_H__
 #define __HTTP_APPLICATION_H__
 
+typedef void (*Callback_func)();
+
 namespace Torch {
 	namespace HTTP {
 		struct application
 		{
-			void get(std::string what) {
-				std::cout << "GET /" << what << "\r\n";
+			void get(std::string what, Callback_func cback) {
+				if(what == "/")
+					cback();
 			}
+			
 			void put();
 			void post();
 
