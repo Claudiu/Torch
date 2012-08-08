fb0c4aeed52c9d241c97325bc64267cc8f5a8c07
Claudiu T <clawgm@gmail.com>
2012-08-08 22:21:39 +0300

    code cleanup

--- include/torch/HTTP/Response/response.hpp
+++ include/torch/HTTP/Response/response.hpp
@@ -2,7 +2,6 @@
 #define _HTTP_RESPONSE_H
 
 #include <torch/HTTP/application.hpp>
-#include <torch/HTTP/Response/response.hpp>
 #include <torch/sockets.hpp>
 #include <torch/logs.hpp>
 #include <set>
@@ -18,34 +17,11 @@ namespace Torch {
 	namespace HTTP {
         class response
         {
+        private:
+        	socket * sock;
         public:
-            response(socket * sock) {
-                this->sock = sock;
-            }
-
-            socket * sock;
-            void send(std::string what) {
-                std::stringstream temp;
-
-                temp << "HTTP/1.1 200 OK\n";
-                temp << "Content-Type: text/html\n";
-                temp << "Content-Length: " << what.length() << "\n";
-                temp << "Connection: close\n";
-                temp << "\n";
-                temp << what;
-
-                std::string t = temp.str();
-                const char * kitty = t.c_str();
-
-                size_t kitty_tail_length = strlen(kitty); 
-                uint8_t * rez = new uint8_t[kitty_tail_length];
-
-                memcpy(rez, kitty, kitty_tail_length);
-
-                Torch::toLog(std::string((char*)rez));
-
-                sock->queue_for_writing(rez, kitty_tail_length);
-            }
+            response(socket*);
+            void send(std::string);
     	};
 	}
 }
---
+++ src/modules/HTTP/Response/response.cpp
@@ -0,0 +1,29 @@
+#include <torch/HTTP/Response/response.hpp>
+#include <torch/sockets.hpp>
+
+Torch::HTTP::response::response(socket * sock) {
+	this->sock = sock;
+}
+
+void Torch::HTTP::response::send(std::string what) {
+	std::stringstream temp;
+
+	temp << "HTTP/1.1 200 OK\n";
+	temp << "Content-Type: text/html\n";
+	temp << "Content-Length: " << what.length() << "\n";
+	temp << "Connection: close\n";
+	temp << "\n";
+	temp << what;
+
+	std::string t = temp.str();
+	const char * kitty = t.c_str();
+
+	size_t kitty_tail_length = strlen(kitty); 
+	uint8_t * rez = new uint8_t[kitty_tail_length];
+
+	memcpy(rez, kitty, kitty_tail_length);
+
+	Torch::toLog("Sending HTTP response back.");
+
+	this->sock->queue_for_writing(rez, kitty_tail_length);
+}
